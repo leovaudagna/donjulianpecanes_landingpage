53b9695445a8b5f431126089e3b52f98fb9b2333
Leonardo <leovaudagna92@gmail.com>
2025-08-26 14:58:04 -0300

    retoques en estructura main

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ window.addEventListener("load", () => {
     setTimeout(() => {
       const splash = document.getElementById("splash-logo");
       splash.style.opacity = 0;      
-      setTimeout(() => splash.style.display = "none", 800);
-    }, 500);
+      setTimeout(() => splash.style.display = "none", 1900);
+    }, 600);
   });
