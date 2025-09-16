cbd391af81a17174779cad30af403462985c348b
Leonardo <leovaudagna92@gmail.com>
2025-09-16 00:26:29 -0300

    Correcciones funcion categorias - producto

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,5 @@
-console.log("HOLIS")
 
-//precios
+//Precios
 const preciosPeladas = {
     "100g": 3500,
     "250g": 7000,
@@ -24,4 +23,49 @@ document.querySelectorAll(".card-producto").forEach(article => {
         });
     });    
 
+});
+
+//FILTRO
+// document.querySelectorAll(".categorias").forEach(article => {
+//     const botonesCategoria = article.querySelectorAll(".categorias button");
+//     const botonPorCategoria = document.getElementById("btn_porCategoria");
+//     const botonTodos = document.getElementById("btn_todos");
+
+//     botonesCategoria.forEach(boton => {
+//         boton.addEventListener("click", () => {
+//             botonesCategoria.forEach(b => b.classList.remove("selected"));
+//             boton.classList.add("selected");
+//         })
+//     })        
+// });
+
+//FUNCION PRUEBA
+document.addEventListener('DOMContentLoaded', () => {
+    const btnPorCategoria = document.getElementById('btn_porCategoria');
+    const btnTodos = document.getElementById('btn_todos');
+    const grilla = document.getElementById('grilla');
+    const todos = document.getElementById('todos');
+
+    btnPorCategoria.addEventListener('click', () => {
+    btnPorCategoria.classList.add('selected');
+    btnTodos.classList.remove('selected');
+
+    grilla.classList.remove('collapsed'); // mostrar grilla
+    todos.classList.add('collapsed');     // ocultar todos
+  });
+
+  btnTodos.addEventListener('click', () => {
+    btnTodos.classList.add('selected');
+    btnPorCategoria.classList.remove('selected');
+
+    todos.classList.remove('collapsed');  // mostrar todos
+    grilla.classList.add('collapsed');    // ocultar grilla
+
+    if(todos.innerHTML.trim() === "") {
+        const articulos = document.querySelectorAll(".container-tipo-producto article");
+        articulos.forEach(art => {
+            todos.appendChild(art.cloneNode(true));
+        })
+    }
+  });
 });
